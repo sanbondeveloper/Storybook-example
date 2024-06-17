eba5354221b529a5f0b3ac25b4fadd626b56a4b2
sanbondeveloper <sanbondeveloper@gmail.com>
2024-06-17 20:58:21 +0900

    ✨ useMemo, useCallback 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function expensiveComputation(a: number, b: number) {
+  return a + b;
 }
 
-export default App
+function ExpensiveComponent({ value }: { value: number }) {
+  useEffect(() => {
+    console.log('rendering!');
+  }, []);
+
+  return <span>{value + 1000}</span>;
+}
+
+function App() {
+  const [value, setValue] = useState(0);
+  const memoizedValue = useMemo(() => expensiveComputation(10, 20), []);
+  const MemoizedComponent = useMemo(
+    () => <ExpensiveComponent value={value} />,
+    [value]
+  );
+
+  // useMemo로 useCallback을 구현하는 경우 다음과 같이 불필요하게 코드가 길어지고 혼동을 야기할 수 있다.
+  const increase = useCallback(function handleClick() {
+    setValue((prev) => prev + 1);
+  }, []);
+
+  return (
+    <div>
+      <button onClick={increase}>증가</button>
+      <div>{memoizedValue}</div>
+      {MemoizedComponent}
+    </div>
+  );
+}
+
+export default App;
